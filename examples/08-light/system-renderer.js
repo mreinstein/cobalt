88abfe7b07f605ea98d92c2ba0a3b916c633df89
Mike <reinstein.mike@gmail.com>
2024-08-27 13:57:58 -0700

    remove unused code from light node example

--- examples/08-light/system-renderer.js
+++ examples/08-light/system-renderer.js
@@ -3,18 +3,11 @@ import Game        from './Game.js'
 import { ECS }     from './deps.js'
 
 
-const SPRITE_QUERY = [ 'sprite' ]
-
-
 // @param Object renderer Cobalt render state
 export default function createRendererSystem (renderer) {
 
     // temporary variables, allocated once to avoid garbage collection
     const buf = new Float32Array(136)  // tile instance data stored in a UBO
-    const removedEntities = {
-        count: 0,
-        entries: new Array(100)
-    }
 
     return function rendererSystem (world) {
     
@@ -22,13 +15,6 @@ export default function createRendererSystem (renderer) {
      
             const device = renderer.device
             const context = renderer.context
-
-            ECS.getEntities(world, SPRITE_QUERY, 'removed', removedEntities)
-
-            for (let i=0; i < removedEntities.count; i++) {
-                const oldSprite = removedEntities.entries[i]
-                oldSprite.spriteNode.removeSprite(oldSprite.sprite.cobaltSpriteId)
-            }
 
             Cobalt.draw(Game.renderer)
         }
