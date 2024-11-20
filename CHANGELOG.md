b18e71620c5a1ddfe0615eab4602fac1bb5fb22c
Mike <reinstein.mike@gmail.com>
2024-11-20 09:38:51 -0800

    0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.2.0
+* refactor primitives node: support transforms, stroked and filled paths
+* added CHANGELOG
--- esbuild.js
+++ esbuild.js
@@ -10,8 +10,6 @@ esbuild.build({
     format: 'esm',
     target: 'es2022',
     plugins: [
-        // minifying used to break the code for some reason, but I think it's fixed
-        // so I'll re-enable this and see how it goes!
         glsl({ minify: true }),
         http()
     ],
